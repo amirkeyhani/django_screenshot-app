64c938fb39308cd27425a39f1a6f037a532a5ea8
CyberSecurity0110 <galaxyman0110@gmail.com>
2022-02-15 15:22:26 +0330

    screenshottaker_app

---
+++ myapp/views.py
@@ -0,0 +1,33 @@
+import re
+from django.shortcuts import render
+import random
+import pyautogui
+from django.conf import settings
+from django.contrib import messages
+
+# def index(request):
+#     if request.method == "POST":
+#         ss = pyautogui.screenshot()
+#         img = f'myimg{random.randint(1000,9999)}.png'
+#         ss.save(settings.MEDIA_ROOT/img)
+#         messages.success(request,'screenshot has been taken')
+#         return render(request,'index.html',{'img':img})
+#     return render(request,'index.html')
+
+# def screenshot(request):
+#     if request.method == "POST":
+#         ss = pyautogui.screenshot()
+#         img = f'myimg{random.randint(1000, 9999)}.png'
+#         ss.save(settings.MEDIA_ROOT/img)
+#         messages.success(request, 'screenshot has been taken')
+#         return render(request, 'index.html', {'img': img})
+#     return render(request, 'index.html')
+
+def screenshot(request):
+    if request.method == "POST":
+        ss = pyautogui.screenshot()
+        img = f'myimg{random.randint(1000, 9999)}.png'
+        ss.save(settings.MEDIA_ROOT/img)
+        messages.success(request, 'screenshot has been taken')
+        return render(request, 'index.html', {'img': img})
+    return render(request, 'index.html')
